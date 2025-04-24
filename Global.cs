0ef941b1e9319a7e1b53d685a772cf106f8d21a7
Alexander Schönberg <alexoshi@live.de>
2025-04-24 23:45:44 +0200

    Structured GUI

---
+++ Global.cs
@@ -0,0 +1,11 @@
+﻿namespace Support
+{
+    public interface IMainWindow
+    {
+        void SetText(string text, LoggingLevel level = LoggingLevel.Info);
+    }
+    public enum LoggingLevel
+    {
+        Info, Warning, Error, Debug
+    }
+}
